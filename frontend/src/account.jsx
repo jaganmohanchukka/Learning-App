177beb295bbbaefbce3cc880327ac2443fabd53d
chukka jagan mohan <jaganchukka@gmail.com>
2026-08-18 11:25:58 +0530

    added logout function

--- frontend/src/account.jsx
+++ frontend/src/account.jsx
@@ -1,30 +1,25 @@
-import React,{useContext ,useState} from 'react'
+import React from 'react'
 import { useAuth } from './auth';
+import { useNavigate } from 'react-router-dom';
 
 function Account(){
-    // const user = useAuth();
-    // const [name, setname]=useState("");
-    // function handlechange(e){ 
-    //     setname(e.target.value);
-    // }
-    // function handleclick(){
-    //     if(name.length!== 0){
-    //         setuser(name);
-    //     }
-        
-    //     setname("");
-    // }
-    // return(
-    //     <div id="outer" >
-    //         <h1>HELLO, {username.name} </h1>
-    //         <div id="searcch-set">
-    //             <input type="text"  id="search" value={name} placeholder='Change User Name' onChange={handlechange} />
-    //             <button id="search-btn" onClick={handleclick} >Change </button>  
-    //         </div>
-    //     </div>
-    // )
+    const {user,logout} = useAuth();
+    const navigate = useNavigate();
+    function handlelogout(){
+        const cofm = window.confirm("do you want to logout");
+        if(cofm){
+            logout();
+            navigate("/");
+        }
+
+    }
+    
     return(
-        <div>account</div>
+        <div id="outer">
+            <p>account</p>
+            <h1>HELLO, {user.name} </h1>
+            <button id="logout" onClick={handlelogout}>logout</button>
+        </div>
     );
 }
 
